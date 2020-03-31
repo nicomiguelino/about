c696d69111e3432d1ea79b9c4e372627ae0f63d0
Nico Miguelino <nicomiguelino2014@gmail.com>
2020-03-31 23:22:12 +0800

    Added smooth scrolling; added button for scrolling down; installed jquery; Added functionality for scrolling up/down on arrow keypress.

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-banner',
@@ -10,6 +11,31 @@ export class BannerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const UP_ARROW_KEY_CODE = 38;
+    const DOWN_ARROW_KEY_CODE = 40;
+    const SCROLL_DELAY = 0;
+
+    let scrollToId = (idSelector) => {
+      $('html, body').animate({
+        scrollTop: $(idSelector).offset().top
+      }, SCROLL_DELAY);
+    };
+
+    $('#scroll-button').click(() => {
+      scrollToId('#intro-section');
+    });
+
+    $('html').keyup(e => {
+      e.preventDefault();
+      switch(e.keyCode) {
+        case UP_ARROW_KEY_CODE:
+          scrollToId('#index-banner');
+          break;
+        case DOWN_ARROW_KEY_CODE:
+          scrollToId('#intro-section');
+        default: break;
+      }
+    });
   }
 
 }
