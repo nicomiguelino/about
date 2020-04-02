e10c5308df55b57ad5607fafa4b35c6a1b81e49d
Nico Miguelino <nicomiguelino2014@gmail.com>
2020-04-02 22:26:58 +0800

    Design and color palette changes. Removed jquery (was an evil idea)

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-banner',
@@ -10,32 +9,5 @@ export class BannerComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-    const UP_ARROW_KEY_CODE = 38;
-    const DOWN_ARROW_KEY_CODE = 40;
-    const SCROLL_DELAY = 0;
-
-    let scrollToId = (idSelector) => {
-      $('html, body').animate({
-        scrollTop: $(idSelector).offset().top
-      }, SCROLL_DELAY);
-    };
-
-    $('#scroll-button').click(() => {
-      scrollToId('#intro-section');
-    });
-
-    $('html').keyup(e => {
-      e.preventDefault();
-      switch(e.keyCode) {
-        case UP_ARROW_KEY_CODE:
-          scrollToId('#index-banner');
-          break;
-        case DOWN_ARROW_KEY_CODE:
-          scrollToId('#intro-section');
-        default: break;
-      }
-    });
-  }
-
+  ngOnInit() { }
 }
